74dc3c6a9febb8cd192c6d6b1f7cf6a0e286df9d
Mike Matthews <matthews.mike@gmail.com>
2020-05-15 16:44:01 -0300

    Google API Key put in the .env file where it belongs.

--- config/googlemaps.php
+++ config/googlemaps.php
@@ -11,7 +11,7 @@ return [
     /* =====================================================================
     General
     ===================================================================== */
-    'key' => env('GOOGLE_MAPS_API_KEY', 'AIzaSyBVJEFkQfB-EVNxrFM_5E2vqImtGrIlfec'), //Get API key: https://code.google.com/apis/console
+    'key' => env('GOOGLE_MAPS_API_KEY', 'YOUR_GOOGLE_MAPS_API_KEY'), //Get API key: https://code.google.com/apis/console
     'adsense_publisher_id' => env('GOOGLE_ADSENSE_PUBLISHER_ID', ''), //Google AdSense publisher ID
 
     'geocode' => [
